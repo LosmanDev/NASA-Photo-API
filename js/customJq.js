47e69570c1c533cf56a8da4b977b1fcb12a9004c
Liban Osman <32774298+LosmanDev@users.noreply.github.com>
2019-12-24 19:56:42 -0500

    Add files via upload
    Just for fun project to further develop javascript skills.

---
+++ js/customJq.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//   $("#buttn").on("click", function() {
+//     var getValue = $("#nasa_input1").val();
+
+//     var apiKey = "chtfxvAvmOZcD6YaskyqKVZXgbZIzW2c34KSW4n0";
+//     var apiURL =
+//       "https://api.nasa.gov/planetary/apod?date=" +
+//       getValue +
+//       "&api_key=" +
+//       apiKey;
+//     $.ajax({
+//       url: apiURL,
+
+//       success: function(r) {
+//         console.log(r);
+//         //   $("img").attr("src", r.url);
+//         //   $(".description").append(r.explanation);
+//       },
+//       error: function(r) {
+//         alert("THE YEAR HAS TO BE 1995 OR GREATER");
+//       }
+//     });
+//   });
+// });
